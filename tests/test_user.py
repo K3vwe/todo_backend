a6887e2975baa1a0d41d223f30570c891d2e117c
k3vwe <rukkimax@gmail.com>
2026-01-14 12:07:09 -0600

    Unit test users bug

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,13 +1,14 @@
-# -----------------------------
-# TEST CREATE USER
-# -----------------------------
-def test_create_user(client):
-    payload ={
+payload ={
         "fullname": "John Doe",
         "username": "jonnydoe",
         "email": "johndoe@gmail.com",
         "hashed_password": "onosnonso#$*@Ynsdf"
     }
+
+# -----------------------------
+# TEST CREATE USER
+# -----------------------------
+def test_create_user(client):
 
     response = client.post('/api/v1/users/', json=payload)
 
@@ -25,12 +26,7 @@ def test_create_user(client):
 def test_get_user_by_id(client):
     create = client.post(
         '/api/v1/users/',
-        json={
-            "fullname": "Fuled",
-            "username": "active",
-            "email": "fuled@gmail.com",
-            "hashed_password": "securehash"
-        }
+        json=payload
     )
 
     assert create.status_code == 201
@@ -41,7 +37,11 @@ def test_get_user_by_id(client):
 
     data = response.json()
     assert data["id"] == user_id
-    assert data["username"] == "active"
+    assert data["username"] == payload["username"]
+    assert data["fullname"] == payload["fullname"]
+    assert data["email"] == payload["email"]
+   
+
 
 
 # -----------------------------
@@ -51,12 +51,7 @@ def test_update_user(client):
     # Step 1: Create user
     create = client.post(
         "/api/v1/users/",
-        json={
-            "fullname": "John Doe",
-            "username": "johndoe",
-            "email": "john@gmail.com",
-            "hashed_password": "hash123"
-        }
+        json=payload
     )
 
     user_id = create.json()["id"]
@@ -77,9 +72,9 @@ def test_update_user(client):
 
     # Step 3: Assertions
     assert data["id"] == user_id
-    assert data["fullname"] == "John Updated"
-    assert data["username"] == "johnny"
-    assert data["email"] == "john@gmail.com"  # unchanged
+    assert data["fullname"] == update_payload["fullname"]
+    assert data["username"] == update_payload["username"]
+    assert data["email"] == payload["email"]  # unchanged
     assert "updated_at" in data
 
 
@@ -90,12 +85,7 @@ def test_delete_user(client):
     # Step 1: Create user
     create = client.post(
         "/api/v1/users/",
-        json={
-            "fullname": "Jane Doe",
-            "username": "janedoe",
-            "email": "jane@gmail.com",
-            "hashed_password": "hash123"
-        }
+        json=payload
     )
 
     # assert create.status_code == 200
@@ -111,3 +101,4 @@ def test_delete_user(client):
     # Step 3: Verify user is gone
     get_deleted = client.get(f"/api/v1/users/{user_id}")
     assert get_deleted.status_code == 404
+
